d3139680c024cd74de49c784e90e95d0f75dd378
44137 <indi.knighton>
2014-09-16 16:01:32 +0100

    revision tasks

---
+++ Revision 1.py
@@ -0,0 +1,10 @@
+#Indi Knighton
+#16-09-2014
+#Four integers added together
+
+number1 = int(input("Please enter a number here: "))
+number2 = int(input("Please enter a number here: "))
+number3 = int(input("Please enter a number here: "))
+number4 = int(input("Please enter a number here: "))
+answer = number1 + number2 + number3 + number4
+print(answer)
---
+++ Revision 2.py
@@ -0,0 +1,9 @@
+#Indi Knighton
+#16-09-2014
+#Revision task 2
+
+number1 = int(input("Please enter a number here: "))
+number2 = int(input("Please enter a number here: "))
+number3 = int(input("Please enter a number here: "))
+answer = (number1 * number2) / number3
+print(answer)
