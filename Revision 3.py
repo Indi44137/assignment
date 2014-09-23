495baf22d880e84612252c3703ba29523da7c7ca
Indi <Indi@192.168.0.10>
2014-09-23 18:04:11 +0100

    This task asks the user for two intergers then diveds the numbers and gives a number and a remainder

---
+++ Revision 3.py
@@ -0,0 +1,11 @@
+#Indi Knighton
+#16/09/2014
+#Revision exercises
+
+number1 = int(input("Please enter a number here: "))
+number2 = int(input("Please enter another number here: "))
+
+div_result = number1//number2
+mod_result = number1%number2
+print("The two numbers divided by each other is {0} remainder {1}!".format(div_result, mod_result))
+
