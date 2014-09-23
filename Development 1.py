6b72626795886a73e747e8c5802cbf99845060e3
Indi <Indi@192.168.0.10>
2014-09-23 18:50:47 +0100

    Development task 1

--- Development 1.py
+++ Development 1.py
@@ -8,4 +8,4 @@ number2 = int(input("Please enter another number here: "))
 number3 = number1//number2
 number4 = number1%number2
 
-print("The answer of number1 divided by number2 is {0} remainder {1}!".format(number1, number2))
+print("The answer of number1 divided by number2 is {0} remainder {1}!".format(number3, number4))
