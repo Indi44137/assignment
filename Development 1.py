b9bd22535cd3cd417b7c3cb19b8848a7f5acac40
Indi <Indi@192.168.0.10>
2014-09-23 18:41:47 +0100

    A task dividing a number and giving the given number and the remainder

---
+++ Development 1.py
@@ -0,0 +1,11 @@
+#Indi Knighton
+#23/09/2014
+#Develpoment Task 1
+
+number1 = int(input("Please enter a number here: "))
+number2 = int(input("Please enter another number here: "))
+
+number3 = number1//number2
+number4 = number1%number2
+
+print("The answer of number1 divided by number2 is {0} remainder {1}!".format(number1, number2))
